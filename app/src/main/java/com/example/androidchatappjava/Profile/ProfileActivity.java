3fb6c2453f75262b7047bada9a83eed86ea185e8
StephenLeeDev <stephen.lee.dev@gmail.com>
2020-12-30 20:09:31 +0900

    CREATE : 'Profile' Firebase Profile UI in Action

---
+++ app/src/main/java/com/example/androidchatappjava/Profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidchatappjava.Profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidchatappjava.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
