4c0551e5cdb47245de2a8d78a6654a259d0adc6d
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-01-20 21:50:25 +0900

    CREATE : Find Friends Implementation(5m)

--- app/src/main/java/com/example/androidchatappjava/FindFriends/FindFriendsFragment.java
+++ app/src/main/java/com/example/androidchatappjava/FindFriends/FindFriendsFragment.java
@@ -2,48 +2,49 @@ package com.example.androidchatappjava.FindFriends;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.androidchatappjava.Common.NodeNames;
 import com.example.androidchatappjava.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FindFriendsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindFriendsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerViewFindFriends;
+    private FindFriendAdapter findFriendAdapter;
+    private List<FindFriendModel> findFriendModelList;
+    private TextView textViewEmptyFriendsList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference reference;
+    private FirebaseUser currentUser;
+    private View progressBar;
 
     public FindFriendsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FindFriendsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FindFriendsFragment newInstance(String param1, String param2) {
         FindFriendsFragment fragment = new FindFriendsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +53,46 @@ public class FindFriendsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_find_friends, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerViewFindFriends = view.findViewById(R.id.recyclerViewFindFriends);
+        progressBar = view.findViewById(R.id.progressBar);
+        textViewEmptyFriendsList = view.findViewById(R.id.textViewEmptyFriendsList);
+
+        recyclerViewFindFriends.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        findFriendModelList = new ArrayList<>();
+        findFriendAdapter = new FindFriendAdapter(getActivity(), findFriendModelList);
+        recyclerViewFindFriends.setAdapter(findFriendAdapter);
+
+        reference = FirebaseDatabase.getInstance().getReference().child(NodeNames.getInstance().USERS);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        textViewEmptyFriendsList.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        Query query = reference.orderByChild(NodeNames.getInstance().NAME);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
