c2dc2204d7baa1135b5d3d2cc9181f1519bd5612
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-01-27 00:04:02 +0900

    CREATE : Friends Request Implementation

--- app/src/main/java/com/example/androidchatappjava/Requests/RequestsFragment.java
+++ app/src/main/java/com/example/androidchatappjava/Requests/RequestsFragment.java
@@ -2,48 +2,50 @@ package com.example.androidchatappjava.Requests;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.androidchatappjava.Common.Constants;
+import com.example.androidchatappjava.Common.NodeNames;
 import com.example.androidchatappjava.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RequestsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class RequestsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private RequestAdapter adapter;
+    private List<RequestModel> list;
+    private TextView textViewEmptyRequestsList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference referenceRequest, referenceUser;
+    private FirebaseUser currentUser;
+    private View progressBar;
 
     public RequestsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RequestsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RequestsFragment newInstance(String param1, String param2) {
+    public static RequestsFragment newInstance() {
         RequestsFragment fragment = new RequestsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +54,78 @@ public class RequestsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_requests, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        textViewEmptyRequestsList = view.findViewById(R.id.textViewEmptyRequestsList);
+        progressBar = view.findViewById(R.id.progressBar);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        list = new ArrayList<>();
+        adapter = new RequestAdapter(getActivity(), list);
+        recyclerView.setAdapter(adapter);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        referenceUser = FirebaseDatabase.getInstance().getReference().child(NodeNames.getInstance().USERS);
+        referenceRequest = FirebaseDatabase.getInstance().getReference().child(NodeNames.getInstance().FRIEND_REQUESTS).child(currentUser.getUid());
+
+        textViewEmptyRequestsList.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        referenceRequest.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                progressBar.setVisibility(View.GONE);
+                list.clear();
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot.exists()) {
+                        String requestType = dataSnapshot.child(NodeNames.getInstance().REQUEST_TYPE).getValue().toString();
+                        if (Constants.getInstance().REQUEST_STATUS_RECEIVED.equals(requestType)) {
+                            String userId = dataSnapshot.getKey();
+                            referenceUser.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot2) {
+                                    String userName = dataSnapshot.child(NodeNames.getInstance().NAME).getValue().toString();
+                                    String photoName = "";
+
+                                    if (dataSnapshot.child(NodeNames.getInstance().PHOTO).getValue() != null) {
+                                        photoName = dataSnapshot.child(NodeNames.getInstance().PHOTO).getValue().toString();
+                                    }
+
+                                    RequestModel model = new RequestModel(userId, userName, photoName);
+                                    list.add(model);
+                                    adapter.notifyDataSetChanged();
+                                    textViewEmptyRequestsList.setVisibility(View.GONE);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(getActivity(), getActivity().getString(R.string.failed_to_fetch_friends, error.getMessage()), Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), getActivity().getString(R.string.failed_to_fetch_friends, error.getMessage()), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
