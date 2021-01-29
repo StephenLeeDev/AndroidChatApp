24944f110db8981e72a7bdb4e25420331f10d21b
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-01-29 23:56:39 +0900

    CREATE : Chat List Adapter

---
+++ app/src/main/java/com/example/androidchatappjava/Chats/ChatListAdapter.java
@@ -0,0 +1,72 @@
+package com.example.androidchatappjava.Chats;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.androidchatappjava.Common.Constants;
+import com.example.androidchatappjava.R;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder> {
+
+    private Context context;
+    private List<ChatListModel> chatModelList;
+
+    public ChatListAdapter(Context context, List<ChatListModel> chatModelList) {
+        this.context = context;
+        this.chatModelList = chatModelList;
+    }
+
+    @NonNull
+    @Override
+    public ChatListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.chat_list_layout, parent, false);
+        return new ChatListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatListViewHolder holder, int position) {
+
+        ChatListModel model = chatModelList.get(position);
+        holder.textViewFullName.setText(model.getUserName());
+
+        StorageReference reference = FirebaseStorage.getInstance().getReference().child(Constants.getInstance().IMAGE_FOLDER + "/" + model.getPhotoName());
+        reference.getDownloadUrl().addOnSuccessListener(uri -> {
+            Glide.with(context).load(uri).placeholder(R.drawable.default_profile).error(R.drawable.default_profile).into(holder.imageViewProfile);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatModelList.size() ;
+    }
+
+    public class ChatListViewHolder extends RecyclerView.ViewHolder {
+
+        private LinearLayout linearLayout;
+        private TextView textViewFullName, textViewLastMessage, textViewUnreadCount, textViewLastMessageTime;
+        private ImageView imageViewProfile;
+
+        public ChatListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            linearLayout = itemView.findViewById(R.id.linearLayout);
+            textViewFullName = itemView.findViewById(R.id.textViewFullName);
+            textViewLastMessage = itemView.findViewById(R.id.textViewLastMessage);
+            textViewUnreadCount = itemView.findViewById(R.id.textViewUnreadCount);
+            textViewLastMessageTime = itemView.findViewById(R.id.textViewLastMessageTime);
+            imageViewProfile = itemView.findViewById(R.id.imageViewProfile);
+        }
+    }
+}
