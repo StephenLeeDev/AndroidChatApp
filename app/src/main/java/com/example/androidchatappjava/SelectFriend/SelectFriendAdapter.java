e40b104ac91d6134d0de6966cee5d5059059d4b0
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-02-12 14:26:24 +0900

    CREATE : Select Friend Adapter

---
+++ app/src/main/java/com/example/androidchatappjava/SelectFriend/SelectFriendAdapter.java
@@ -0,0 +1,81 @@
+package com.example.androidchatappjava.SelectFriend;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.androidchatappjava.Common.Constants;
+import com.example.androidchatappjava.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+public class SelectFriendAdapter extends RecyclerView.Adapter<SelectFriendAdapter.SelectFriendViewHolder> {
+
+    private Context context;
+    private List<SelectFriendModel> friendModelList;
+
+
+    public SelectFriendAdapter(Context context, List<SelectFriendModel> friendModelList) {
+        this.context = context;
+        this.friendModelList = friendModelList;
+    }
+
+    @NonNull
+    @Override
+    public SelectFriendAdapter.SelectFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.select_friend_layout, parent,false);
+        return new SelectFriendViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SelectFriendAdapter.SelectFriendViewHolder holder, int position) {
+
+        final SelectFriendModel friendModel = friendModelList.get(position);
+        holder.textViewFullName.setText(friendModel.getUserName());
+
+        StorageReference photoRef = FirebaseStorage.getInstance().getReference().child(Constants.getInstance().IMAGE_FOLDER + "/" + friendModel.getPhotoName());
+
+        photoRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(context)
+                        .load(uri)
+                        .placeholder(R.drawable.default_profile)
+                        .error(R.drawable.default_profile)
+                        .into(holder.imageViewProfile);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return friendModelList.size();
+    }
+
+    public class SelectFriendViewHolder extends  RecyclerView.ViewHolder {
+
+        private LinearLayout linearLayoutSelectFriend;
+        private ImageView imageViewProfile;
+        private TextView textViewFullName;
+
+        public SelectFriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            linearLayoutSelectFriend = itemView.findViewById(R.id.linearLayoutSelectFriend);
+            imageViewProfile = itemView.findViewById(R.id.imageViewProfile);
+            textViewFullName = itemView.findViewById(R.id.textViewFullName);
+        }
+    }
+}
